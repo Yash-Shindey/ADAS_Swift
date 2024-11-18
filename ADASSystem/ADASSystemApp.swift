3c6202891f234e66fd8e1709f48a6412019f8d46
Yash Shindey <yashshindey1@gmail.com>
2024-11-18 18:09:33 +0530

    Initial Commit

---
+++ ADASSystem/ADASSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADASSystemApp.swift
+//  ADASSystem
+//
+//  Created by Yash Shindey on 18/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ADASSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ADASSystemTests/ADASSystemTests.swift
@@ -0,0 +1,17 @@
+//
+//  ADASSystemTests.swift
+//  ADASSystemTests
+//
+//  Created by Yash Shindey on 18/11/24.
+//
+
+import Testing
+@testable import ADASSystem
+
+struct ADASSystemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
